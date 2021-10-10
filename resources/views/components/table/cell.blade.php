a21604d3c343c2a9420a4c5509230457c045c33d
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-10-09 22:51:12 -0500

    Success Message Added

--- resources/views/components/table/cell.blade.php
+++ resources/views/components/table/cell.blade.php
@@ -1,3 +1,3 @@
-<td {{ $attributes }} class="px-6 py-4 whitespace-nowrap">
+<td  {{ $attributes->merge(['class' => 'px-6 py-4 whitespace-nowrap']) }}>
     <div class="text-sm text-gray-500">{{ $slot }}</div>
 </td>
--- resources/views/layout/employee.blade.php
+++ resources/views/layout/employee.blade.php
@@ -11,9 +11,6 @@
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="sweetalert2.all.min.js"></script>
-    <script src="sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="sweetalert2.min.css">
 </head>
 <body>
 <x-modal :brands=" $brands = \App\Models\Bike\Bike_Brand::all()->toArray() "
@@ -23,11 +20,12 @@
     <x-swal.success message="{{ session('success') }}" />
 @endif
 
-<div class="flex justify-start w-full">
-    <div>
+<div class="flex justify-start h-screen w-full">
+    <div class=" h-screen">
         <x-employee_sidebar/>
     </div>
-    <div class="w-screen h-screen overflow-y-scroll">
+
+    <div class="w-screen h-screen overflow-y-auto">
         <x-employee_navi/>
         <div class="">
             @yield('content')
