585a151aa32316723630ed74d10a45a7aa44d022
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-10-11 09:39:04 -0500

    Stats On Employee Dashboard Functional

--- resources/views/components/Links/Link.blade.php
+++ resources/views/components/Links/Link.blade.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <link rel="stylesheet" href="{{ asset('css/toggle.css') }}">
 <link rel="stylesheet" href="{{ asset('css/active.css') }}">
+<link href="https://kit-pro.fontawesome.com/releases/v5.15.4/css/pro.min.css" rel="stylesheet">
 
 <script defer src="{{ asset('js/upload.js') }}"></script>
 
