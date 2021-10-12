fcd9dcfce46ec5b919ac6d0c69e895ce2ffe5124
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-10-11 23:26:04 -0500

    Adjusted For Search By Model Functionality

--- app/Http/Controllers/Employee/Clients/ClientsController.php
+++ app/Http/Controllers/Employee/Clients/ClientsController.php
@@ -2,23 +2,26 @@
 
 namespace App\Http\Controllers\Employee\Clients;
 
+use App\Actions\SearchBikeAction;
 use App\Actions\ShowCustomerAction;
 use App\Actions\ViewCurrentCustomerAction;
 use App\Actions\ViewPastCustomerAction;
 use App\Http\Controllers\Controller;
-use App\Models\Payment\Rental;
 use Illuminate\Http\Request;
 
 class ClientsController extends Controller
 {
     public function index(ViewPastCustomerAction $PastCustomer,
-                          ViewCurrentCustomerAction $CurrentCustomer)
+                          ViewCurrentCustomerAction $CurrentCustomer,
+                          SearchBikeAction $SearchBike,Request $request)
     {
         $currents = $CurrentCustomer->execute();
         $pasts = $PastCustomer->execute();
+        $bikes = $SearchBike->execute($request);
+
 
 //        dd($customers);
-        return view('Employee.clients.lists.index', compact(['pasts','currents']));
+        return view('Employee.clients.lists.index', compact(['pasts','currents','bikes']));
     }
 
     public function show($id, ShowCustomerAction $showCustomer)
--- routes/Owner/Owner.php
+++ routes/Owner/Owner.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Employee\Bike\BikeStatusController;
 use App\Http\Controllers\Employee\BikeInfoController;
 use App\Http\Controllers\Employee\Clients\ClientsController;
-use App\Http\Controllers\Employee\CustomerInfoController;
 use App\Http\Controllers\Employee\EmployeeDashController;
 use App\Models\Payment\Penalty;
 
@@ -11,11 +10,10 @@ use App\Models\Payment\Penalty;
 Route::resource('Owner', EmployeeDashController::class);
 
 Route::group(['as' => 'Owner'], function () {
-    Route::resource('Customer-list', CustomerInfoController::class);
     Route::resource('Bike', BikeInfoController::class);
     Route::resource('Status', BikeStatusController::class);
 
     Route::group(['as' => 'Customer.'], function () {
-        Route::resource('Past', ClientsController::class);
+        Route::resource('list', ClientsController::class);
     });
 });
