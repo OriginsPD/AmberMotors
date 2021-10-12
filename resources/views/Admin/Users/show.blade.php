d0f20e4440095d4f0eb277163714f1ea43558300
Prokushxx <takinyene@gmail.com>
2021-10-12 17:40:06 -0500

    searching like google

--- resources/views/Admin/Users/show.blade.php
+++ resources/views/Admin/Users/show.blade.php
@@ -5,7 +5,6 @@
 <x-head class="text-center mt-2">Rental History</x-head>
 <div class="flex container mx-auto mt-20 flex-col">
     <div class="-my-2 overflow-x-auto ">
-
         <x-table>
             <x-slot name="heading">
                 <x-table.heading> User ID </x-table.heading>
@@ -29,8 +28,6 @@
                 </x-table.row>
             @endforeach
         </x-table>
-
     </div>
 </div>
-
 @endsection
