4288906d0ee6c0cd91cc0918a8aa4126cc598b2d
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-10-13 13:18:11 -0500

    Income amount display 80% Complete

--- resources/views/components/sidebar-link-create.blade.php
+++ resources/views/components/sidebar-link-create.blade.php
@@ -1,3 +1,3 @@
 <li class="relative bottom-0">
-    <button {{ $attributes->merge(['class' => 'block px-4 cursor-pointer py-2 mt-2 w-10/12 mx-auto text-white px-4 py-1 rounded-xl mx-2 focus:scale-95 duration-75 transform border-white shadow-xl focus:shadow-outline font-medium text-center']) }} >{{ $slot }}</button>
+    <button {{ $attributes->merge(['class' => 'block px-4 cursor-pointer py-2 mt-2 w-10/12 mx-auto text-white px-4 py-1 rounded-md mx-2 hover:scale-105 focus:scale-95 transition duration-500 ease-in-out transform border-white focus:shadow-outline font-medium text-center']) }} >{{ $slot }}</button>
 </li>
